77270bf7dab3c28b8265cfae7608ade0bb18816f
Debanjan Sen <deanjansen32@gmail.com>
2026-06-14 17:07:31 +0530

    Add Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
       <h2>B.Tech CSE Student | DevOps Enthusiast | Exploring Cloud, CI/CD, and Automation.</h2>
 
-      <h3>Github . Linkedin . Email</h3>
+      <h3>GitHub . Linkedin . Email</h3>
 
       <p>
         Passionate about DevOps, cloud technologies, and building production-ready systems.
@@ -68,7 +68,7 @@ function App() {
       >
         <h2>Skills</h2>
 
-        <div className="skill-container">
+        <div className="skills-container">
           <div className="skill-card">
             <h3>DevOps</h3>
             <p>Docker • Kubernetes • GitHub Actions • Linux • Jenkins</p>
@@ -81,27 +81,56 @@ function App() {
 
           <div className="skill-card">
             <h3>Cloud</h3>
-            <p>AWS • Ngnix</p>
+            <p>AWS • Nginx</p>
           </div>
 
           <div className="skill-card">
-            <h3>frontend</h3>
+            <h3>Frontend</h3>
             <p>React • HTML • CSS</p>
           </div>
         </div>
       </section>
 
     {/* Temporary Projects Section */}
-    <div
+    <section
       id="projects"
       style={{
-        height: "100vh",
+        minHeight: "100vh",
         padding: "50px",
+        textAlign: "center"
       }}
     >
-      <h2>Projects Section</h2>
-      <p>Your future projects will appear here.</p>
-    </div>
+      <h2>Projects</h2>
+
+      <div className="projects-container">
+        <div className="project-card">
+          <h3>DevOps Portfolio website</h3>
+          <p>
+            A professional website documenting my journey into DevOps and software engineering.
+          </p>
+          <p><strong>Tech:</strong> React, Vite, Git, GitHub</p>
+      </div>
+
+        <div className="project-card">
+          <h3>Expense Tracker</h3>
+          <p>
+            Web application to track income and expenses with budget alerts and
+            visual insights.
+          </p>
+          <p><strong>Tech:</strong> React, JavaScript, Charts</p>
+        </div>
+
+        <div className="project-card">
+          <h3>Upcoming DevOps Project</h3>
+          <p>
+            A production-grade project showcasing Docker, CI/CD, AWS, and
+            monitoring.
+          </p>
+          <p><strong>Tech:</strong> Docker, GitHub Actions, AWS</p>
+        </div>
+      </div>
+    </section>
+
     </>
   );
 }
