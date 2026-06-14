4bc9bd00d69fcfb2c7eeb7d1831997eb0221484c
Debanjan Sen <deanjansen32@gmail.com>
2026-06-14 13:27:39 +0530

    Create hero section for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import "./index.css";
+
 function App() {
   return (
-    <div>
-      <h1>Debanjan Sen</h1>
-      <p>Future DevOps Engineer</p>
+    <div className="hero">
+      <h1>Hi, I'm Debanjan Sen 👋</h1>
+
+      <h2>Aspiring DevOps Engineer | Software Developer</h2>
+
+      <p>
+        Building scalable systems and learning by creating.
+      </p>
+
+      <div className="hero-buttons">
+        <button>View Projects</button>
+        <button>Download Resume</button>
+      </div>
     </div>
   );
 }
