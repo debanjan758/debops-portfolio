86472c8394d497b35cfaaf73889c8a77a2ac637d
Debanjan Sen <deanjansen32@gmail.com>
2026-06-14 14:39:23 +0530

    Add About Me Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,70 @@ import "./index.css";
 
 function App() {
   return (
+    <>
     <div className="hero">
       <h1>Hi, I'm Debanjan Sen 👋</h1>
 
-      <h2>Aspiring DevOps Engineer | Software Developer</h2>
+      <h2>B.Tech CSE Student | DevOps Enthusiast | Exploring Cloud, CI/CD, and Automation.</h2>
+
+      <h3>Github . Linkedin . Email</h3>
 
       <p>
-        Building scalable systems and learning by creating.
+        Passionate about DevOps, cloud technologies, and building production-ready systems.
       </p>
 
+    {/* Hero Section */}
+
       <div className="hero-buttons">
-        <button>View Projects</button>
-        <button>Download Resume</button>
+        <a href="#about">
+          <button>About Me</button>
+        </a>
+
+        <a href="#projects">
+          <button>View Projects</button>
+        </a>
+        
+        <a href="/resume.pdf" download>
+          <button>Download Resume</button>
+        </a>
       </div>
     </div>
+
+    <section
+      id="about"
+      style={{
+        minHeight: "100vh",
+        padding: "50px",
+        textAlign: "center"
+      }}
+      >
+        <h2>About Me</h2>
+
+        <p
+          style={{
+            maxWidth: "700px",
+            margin: "20px auto",
+            lineHeight: "1.8",
+            fontSize: "1.1rem",
+          }}        
+        >
+          I'm a B.Tech CSE student with a strong interest in DevOps and cloud computing. I enjoy solving real-world problems through technology and continuously challenge myself by building production-ready projects while learning modern development practices.
+
+        </p>
+      </section>
+
+    {/* Temporary Projects Section */}
+    <div
+      id="projects"
+      style={{
+        height: "100vh",
+        padding: "50px",
+      }}
+    >
+      <h2>Projects Section</h2>
+      <p>Your future projects will appear here.</p>
+    </div>
+    </>
   );
 }
 
